9010d38da3514c4bc731cdc3e62d048077d98b2a
ShuntaSakai <shuntasakai1224@icloud.com>
2026-04-12 22:09:35 +0900

    ALDS1_5_B マージソート(#3)

--- alds15/b/a.cpp
+++ alds15/b/a.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++.h>
 #include <iostream>
-#include <sys/wait.h>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 
 using ll = long long;
@@ -9,49 +10,54 @@ using vi = vector<int>;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define all(x) (x).begin(), (x).end()
 #define MAX 100000
+#define SENTINEL 1000000001
 
-struct Point {
-    double x, y;
-};
+int n, ans;
+vi A;
 
-void kochCurve(int d, Point p1, Point p2) {
-    if(d == 0) {
-        return;
+void merge(vi &A, int left, int mid, int right) {
+    int n1 = mid - left, n2 = right - mid;
+    vi L(n1+1), R(n2+1);
+    rep(i, n1) L[i] = A[left + i];
+    rep(i, n2) R[i] = A[mid + i];
+    L[n1] = R[n2] = SENTINEL;
+
+    int i = 0, j = 0;
+    for (int k=left; k<right; k++) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i++];
+            ans++;
+        } else {
+            A[k] = R[j++];
+            ans++;
+        }
     }
+}
 
-    Point s, t, u;
-    double angle = M_PI * 60.0 / 180.0;
-
-    s.x = (2*p1.x + p2.x) / 3;
-    s.y = (2*p1.y + p2.y) / 3;
-    t.x = (p1.x + 2*p2.x) / 3;
-    t.y = (p1.y + 2*p2.y) / 3;
-    u.x = (t.x - s.x) * cos(angle) - (t.y - s.y) * sin(angle) + s.x;
-    u.y = (t.x - s.x) * sin(angle) + (t.y - s.y) * cos(angle) + s.y;
-
-    kochCurve(d-1, p1, s);
-    printf("%.8f %.8f\n", s.x, s.y);
-    kochCurve(d-1, s, u);
-    printf("%.8f %.8f\n", u.x, u.y);
-    kochCurve(d-1, u, t);
-    printf("%.8f %.8f\n", t.x, t.y);
-    kochCurve(d-1, t, p2);
+void mergeSort(vi &A, int left, int right) {
+    if (left + 1 < right) {
+        int mid = (left + right) / 2;
+        mergeSort(A, left, mid);
+        mergeSort(A, mid, right);
+        merge(A, left, mid, right);
+    }
 }
 
 int main() {
-    Point p1, p2;
-    int n;
-
     scanf("%d", &n);
+    A.resize(n);
+    rep(i, n) {
+        scanf("%d", &A[i]);
+    }
 
-    p1.x = 0;
-    p1.y = 0;
-    p2.x = 100;
-    p2.y = 0;
+    mergeSort(A, 0, n);
 
-    printf("%.8f %.8f\n", p1.x, p1.y);
-    kochCurve(n, p1, p2);
-    printf("%.8f %.8f\n", p2.x, p2.y);
+    rep(i, n) {
+        if (i) printf(" ");
+        printf("%d", A[i]);
+    }
+    printf("\n");
+    printf("%d\n", ans);
 
     return 0;
 }
